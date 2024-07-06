0bbc043378d2ed3ed854de903fbe2d3554b053bc
skylatian <53362705+skylatian@users.noreply.github.com>
2024-07-06 00:55:44 -0700

    move

--- just_one.py
+++ just_one.py
@@ -1,9 +1,12 @@
 from functions.puzzle_processing import get_puzzle
 from pprint import pprint
+from config import *
 
 single_date = "2024-06-18" #"2024-04-06"
 
-emoji, playdata, puzzledata, status = get_puzzle(single_date)
+cookie = user1.cookie
+
+emoji, playdata, puzzledata, status, guesses = get_puzzle(cookie, single_date)
 
 #print(emoji,status)
 
@@ -13,3 +16,5 @@ pprint(playdata)
 #game_data = playdata['states'][0]['game_data']
 
 #solved_in = game_data['currentRowIndex']
+print('-----------------')
+print(emoji)
